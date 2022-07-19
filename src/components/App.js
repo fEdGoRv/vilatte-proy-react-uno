9d9231d522fb376f5565c5f4b4d93d9b8a84f73a
fEdGoRv <vilatte612@gmail.com>
2022-07-19 16:55:34 -0300

    segundo desafio

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Header from "./Header"
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main>
+                <h2>Home</h2>
+            </main>
+            <footer>
+                <p>Soy El Footer</p>
+            </footer>
+        </>
+    )
+}
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <header id="header">
+                <h1 className="tittle">Soy El Titulo</h1>
+                <nav>
+                    <a href="#">Enlace</a>
+                    <a href="#">Enlace2</a>
+                </nav>
+            </header>
+        </>
+    )
+}
+export default Header;
